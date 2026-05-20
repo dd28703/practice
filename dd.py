c9fe9291a35b98bc50807e903d0ec324603a6413
Divyadharshini S <23uea011@avinuty.ac.in>
2026-05-20 13:04:42 +0530

    Create dd.py

---
+++ dd.py
@@ -0,0 +1,12 @@
+# About Me
+def about_me():
+    name = "Divyadharshini S"
+    college = "Avinashilingam Institute"
+    department = "Artificial Intelligence and Data Science"
+    internship = "Vicharanashala Summer internship 2026"
+    print(f"Name: {name}")
+    print(f"College: {college}")
+    print(f"Department: {department}")
+    print(f"Internship: {internship}")
+    print("Goal: To learn, grow and contribute to open source!")
+about_me()
